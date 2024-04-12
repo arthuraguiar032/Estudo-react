22636d576e515f4d1d6bbf91f54f9ed9a91af38c
aguiar.arthur <aguiar.arthur@graduacao.uerj.br>
2024-04-12 17:16:20 -0300

    Atualiza o controle de rotas usando o react-router

--- src/App.jsx
+++ src/App.jsx
@@ -1,33 +1,20 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 import Navbar from './components/navbar'
 import Home from './pages/home'
 import About from './pages/about'
 import Contact from './pages/contact'
+import { Routes, Route } from 'react-router-dom'
  
 function App() {
-  let component
-
-  switch (window.location.pathname) {
-    case '/':
-      component = <Home />
-      break
-    case '/about':
-      component = <About />
-      break
-    case '/contact':
-      component = <Contact />
-      break
-  }
-
   return (
     <>
       <Navbar />
-      {component}     
+      <Routes>
+        <Route path='/' element={<Home />} />
+        <Route path='/about' element={<About />} />
+        <Route path='/contact' element={<Contact />} />
+      </Routes>
     </>
-
   )
 }
  
--- src/components/navbar.jsx
+++ src/components/navbar.jsx
@@ -1,11 +1,14 @@
+import {Link} from 'react-router-dom';
+
+
 export default function Navbar() {
     return (
         <>
         <nav className="nav">
             <ul>
-                <li><a href="/">Home</a></li>
-                <li><a href="/about">Sobre</a></li>
-                <li><a href="/contact">Contato</a></li>
+                <li><Link to="/">Home</Link></li>
+                <li><Link to="/about">Sobre</Link></li>
+                <li><Link to="/contact">Contato</Link></li>
             </ul>
         </nav>
         </>
