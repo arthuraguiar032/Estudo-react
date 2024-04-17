2d3a09b04c4299c0633ffbf53571350b3d568d9f
aguiar.arthur <aguiar.arthur@graduacao.uerj.br>
2024-04-17 14:24:56 -0300

    Criando um formulario com primereact

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -1,21 +1,25 @@
+import { Button } from 'primereact/button';
+import { InputText } from 'primereact/inputtext';
+import {Calendar} from 'primereact/calendar';
+
 export default function Contact() {
     return (
         <>
             <h1>Contato</h1>
             <form id='form'>
-            <label>Nome: <input type="text" placeholder="Digite seu nome"/></label>
+            <label>Nome: <InputText type="text" id='name' placeholder="Digite seu nome"/></label>
             
-            
-            <label>Email: <input type="email" id='email' placeholder="Digite seu email"/></label>
-            
-
-            <label>Matrícula: <input type="number" placeholder="Digite sua matrícula"/></label>
+            <label>Email: <InputText type="email" id='email' placeholder="Digite seu email"/></label>
             
 
-            <label>Data de Nascimento: <input type="date" placeholder="Digite sua data de nascimento"/></label>
+            <label>Matrícula: <InputText type='int' keyfilter="int" placeholder="Somente números" /></label>  
+
+            {/* <label>Data de Nascimento: <input type="date" placeholder="Digite sua data de nascimento"/></label> */}
+            <label>Data de Nascimento: <Calendar id="calendar" placeholder="Escolha uma data" /></label>
+            {/* <Calendar value={date} onChange={(e) => setDate(e.value)} showIcon /> */}
             
             <br />
-            <button type="submit">Enviar</button>
+            <Button label="Enviar" />
         </form>
 
         </>
